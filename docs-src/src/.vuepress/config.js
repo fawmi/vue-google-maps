b11b64d98e3c56de7de7e9954d2feca585d6da6f
Fawad Mirzad <mirzad@arndtteunissen.de>
2020-10-16 18:04:21 +0200

    Fix javascript and css assets cannot be loaded for docs

--- docs-src/src/.vuepress/config.js
+++ docs-src/src/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: description,
-
+  base: '/vue-google-maps/',
   /**
    * Extra tags to be injected to the page HTML `<head>`
    *
