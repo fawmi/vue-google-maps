1a83085a805b57d77c36cb4f27dedee97ac5afee
Fawad Mirzad <mirzad@arndtteunissen.de>
2021-02-13 19:33:37 +0100

    Improve code

--- src/utils/simulateArrowDown.js
+++ src/utils/simulateArrowDown.js
@@ -2,17 +2,17 @@
 // http://stackoverflow.com/a/11703018/2694653
 // This has been ported to Vanilla.js by GuillaumeLeclerc
 export default (input) => {
-  var _addEventListener = input.addEventListener ? input.addEventListener : input.attachEvent
+  const _addEventListener = input.addEventListener ? input.addEventListener : input.attachEvent
 
   function addEventListenerWrapper(type, listener) {
     // Simulate a 'down arrow' keypress on hitting 'return' when no pac suggestion is selected,
     // and then trigger the original listener.
     if (type === 'keydown') {
-      var origListener = listener
+      const origListener = listener
       listener = function (event) {
-        var suggestionSelected = document.getElementsByClassName('pac-item-selected').length > 0
+        const suggestionSelected = document.getElementsByClassName('pac-item-selected').length > 0
         if (event.which === 13 && !suggestionSelected) {
-          var simulatedEvent = document.createEvent('Event')
+          const simulatedEvent = document.createEvent('Event')
           simulatedEvent.keyCode = 40
           simulatedEvent.which = 40
           origListener.apply(input, [simulatedEvent])
