3cf92f1344a67530fc70247c93a5fa9a034ca6ed
Fawad Mirzad <fawmi@users.noreply.github.com>
2021-09-13 12:14:56 +0200

    Add global module definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,1 @@
-/**
- * Sums up two numbers.
- *
- * @param {number} a The first number to add.
- * @param {number} b The second number to add.
- * @returns {number} The resulting sum of the two numbers.
- */
-export declare function add(a: number, b: number): number
-/**
- * Sums up a list of numbers.
- *
- * @param {number[]} numbers An array of numbers to add.
- * @returns {number} The resulting sum of all the numbers.
- */
-export declare function add(numbers: number[]): number
+declare module '@fawmi/vue-google-maps';
